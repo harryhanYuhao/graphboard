39ecd561b557bf6b9f5d64890722c60892197eda
Yuhao Han <116114033+harryhanYuhao@users.noreply.github.com>
2026-08-11 21:06:45 +0100

    rust cartographer submodule and vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,5 +23,8 @@ export default defineConfig({
     setupFiles: ["./vitest.setup.ts"],
     include: ["src/**/*.test.{ts,tsx}"],
     css: false,
+    coverage: {
+      provider: 'v8'
+    },
   },
 });
